bad96a1c92e87348f04c3be4c9aa68c8b25cddc6
Alisson Machado <aamachado@inf.ufsm.br>
2025-02-14 14:26:22 -0300

    feat: add tests to list and search accounts - GREEN
    coveralls: 88%

---
+++ test/dreams_bank_api/accounts/listing_and_searching_test.exs
@@ -0,0 +1,39 @@
+defmodule DreamsBankApi.Accounts.ListingAndSearchingTest do
+  use DreamsBankApi.DataCase
+
+  alias DreamsBankApi.Accounts
+
+  describe "listing accounts" do
+    test "list_accounts/0 returns a list of accounts" do
+      accounts = insert_list(3, :account)
+
+      assert accounts == Accounts.list_accounts()
+    end
+  end
+
+  describe "searching accounts" do
+    test "get_account!/1 returns an account" do
+      account = insert(:account)
+
+      assert account == Accounts.get_account!(account.id)
+    end
+
+    test "get_account!/1 raises Ecto.NoResultsError if account does not exist" do
+      assert_raise Ecto.NoResultsError, fn ->
+        Accounts.get_account!(Faker.UUID.v4())
+      end
+    end
+
+    test "get_account_by_number!/1 returns an account" do
+      account = insert(:account)
+
+      assert account == Accounts.get_account_by_number!(account.number)
+    end
+
+    test "get_account_by_number!/1 raises Ecto.NoResultsError if account does not exist" do
+      assert_raise Ecto.NoResultsError, fn ->
+        Accounts.get_account_by_number!("123")
+      end
+    end
+  end
+end
